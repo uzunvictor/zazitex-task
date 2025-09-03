d11e9032b71dda4ed0487ec2d36f897218f4babd
uzunvictor <v.lungu@webdelo.com>
2025-09-03 21:19:54 +0300

    configuring page transition and primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
+import Aura from '@primeuix/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  css: ['~/assets/scss/index.scss'],
+  devtools: {enabled: true},
+  image: {},
+  modules: [
+    '@primevue/nuxt-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.my-app-dark',
+        }
+      }
+    }
+  }
 })
