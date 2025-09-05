8fd750b1b151ef5ed8b5017d84bf90d299c2fde5
uzunvictor <v.lungu@webdelo.com>
2025-09-05 20:22:18 +0300

    installing firebase and vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,16 +3,17 @@ import Aura from '@primeuix/themes/aura';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: {name: 'page', mode: 'out-in'}
   },
   compatibilityDate: '2025-07-15',
   css: ['~/assets/scss/index.scss'],
   devtools: {enabled: true},
   image: {},
   modules: [
-    '@primevue/nuxt-module',
-    '@nuxtjs/tailwindcss',
     '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+    '@vueuse/nuxt',
   ],
   primevue: {
     options: {
