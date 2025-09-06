d60b217874929394e521b97b5442859d4ce402b2
uzunvictor <v.lungu@webdelo.com>
2025-09-06 13:38:20 +0300

    setting default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="min-h-screen bg-gray-50">
+    <Header/>
+    <div class="container mx-auto">
+        <slot/>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
